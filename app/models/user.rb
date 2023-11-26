e299cb71846b366856b87f4504e32da8247cec4a
ven-b <bigstarbag@gmail.com>
2023-11-26 17:22:54 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,:last_name,:last_kana_name,:first_name,:first_kana_name,:birthday presence: true
+
 end
