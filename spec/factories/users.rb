5f616bf6642e6d7d0c6e5d72323d52675d7a0669
ven-b <bigstarbag@gmail.com>
2023-12-02 19:11:58 +0900

    Factoriesの作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname {Faker::Name.name}
+    email {Faker::Internet.email}
+    password {Faker::Internet.password(min_length: 6)}
+    encrypted_password {password}
+    last_name {"山田"}
+    last_kana_name {"ヤマダ"}
+    first_name {"太郎"}
+    first_kana_name {"タロウ"}
+    birthday {Faker::Date.between(from: '1940-01-10', to: '2017-12-31')}
   end
 end
