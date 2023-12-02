e69c19862904f51b8d641f82b174619dd70ec677
ven-b <bigstarbag@gmail.com>
2023-12-02 17:36:22 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,:last_name,:last_kana_name,:first_name,:first_kana_name,:birthday, presence: true
+  validates :nickname,:birthday,               presence: true
+  validates :last_name,:first_name,           format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }, presence: true
+  validates :last_kana_name,:first_kana_name, format: { with: /\A[ァ-ヶー]+\z/ }, presence: true
+  validates :password,                        format: { with: /\A(?=.*[a-z])(?=.*[\d])[a-z\d]+\z/i }
 
 end
