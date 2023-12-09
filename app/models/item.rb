f4e044a5e7570653f5f1d841820d3269e9e6bc67
ven-b <bigstarbag@gmail.com>
2023-12-09 19:31:07 +0900

    priceカラムのバリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,6 +12,6 @@ class Item < ApplicationRecord
 
   validates :image, :name, :content, presence: true
   validates :category_id,:situation_id,:fee_id,:area_id,:delivery_id,  numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price, format: { with: /[3-9][0-9]{2}|[1-9][0-9]{3,6}/ }, presence: true
+  validates :price, numericality: {greater_than: 299,less_than: 10000000}, presence: true
 
 end
