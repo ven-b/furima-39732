946a23badf450c7f91db87296e50ee6ea852dbbd
ven-b <bigstarbag@gmail.com>
2023-12-09 16:36:42 +0900

    コントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,6 +9,18 @@ class ItemsController < ApplicationController
   end
 
   def create
-
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image,:name,:content,:category_id,:situation_id,:fee_id,:area_id,:delivery_id,:price).merge(user_id: current_user.id)
+  end
+
 end
