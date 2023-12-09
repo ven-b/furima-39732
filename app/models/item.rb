00a4f9808d53f90242251ed32a112aae450ced54
ven-b <bigstarbag@gmail.com>
2023-12-09 15:45:33 +0900

    バリデーションの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,5 +10,8 @@ class Item < ApplicationRecord
   belongs_to :area
   belongs_to :delivery
 
-  
+  validates :name, :content, presence: true
+  validates :category_id,:situation,:fee,:area,:delivery,  numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price, format: { with: /[3-9][0-9]{2}|[1-9][0-9]{3,6}/ }, presence: true
+
 end
