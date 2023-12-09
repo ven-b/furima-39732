1e7fb86a1600a60ca1b95200a327014e62799dca
ven-b <bigstarbag@gmail.com>
2023-12-09 15:14:29 +0900

    ActiveHashの各モデル記載

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,12 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :situation
+  belongs_to :fee
+  belongs_to :area
+  belongs_to :delivery
 
+  
 end
