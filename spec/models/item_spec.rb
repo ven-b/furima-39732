c92aec32f9051a148511c1c5985356a53206892b
ven-b <bigstarbag@gmail.com>
2023-12-09 18:35:36 +0900

    テストモデルの記述

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe '商品出品機能' do
+    context  '商品が出品できる場合' do
+      it '全ての情報が存在すれば登録できる' do
+      end
+    end
+
+    context  '商品が出品できない場合' do
+      it 'imageが空では登録できない' do
+      end
+      it 'nameが空では登録できない' do
+      end
+      it 'category_idが初期値(1)では登録できない' do
+      end
+      it 'situation_idが初期値(1)では登録できない' do
+      end
+      it 'fee_idが初期値(1)では登録できない' do
+      end
+      it 'area_idが初期値(1)では登録できない' do
+      end
+      it 'delivery_idが初期値(1)では登録できない' do
+      end
+      it 'priceが空では登録できない' do
+      end
+      it 'priceが300以下では登録できない' do
+      end
+      it 'priceが9999999以上では登録できない' do
+      end
+      it 'priceが半角数値以外では登録できない' do
+      end
+      it 'userが紐づいていないと登録できない' do
+      end
+    end
+  end
 end
