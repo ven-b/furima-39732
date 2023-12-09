70c23a2b054baa75f5f51358845b77711a4161c3
ven-b <bigstarbag@gmail.com>
2023-12-09 18:39:09 +0900

    テストコード「商品が出品できる場合」

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,10 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
   describe '商品出品機能' do
     context  '商品が出品できる場合' do
       it '全ての情報が存在すれば登録できる' do
+        expect(@item).to be_valid
       end
     end
 
