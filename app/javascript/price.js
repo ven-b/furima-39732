812f0df7c644152049d0df66e976175cf85d75c3
ven-b <bigstarbag@gmail.com>
2023-12-10 14:36:57 +0900

    販売手数料や販売利益の表記変更

---
+++ app/javascript/price.js
@@ -0,0 +1,13 @@
+window.addEventListener('turbo:load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+
+    const addTaxDom = document.getElementById("add-tax-price");
+    const taxPrice = Math.floor(inputValue*0.1)
+    addTaxDom.innerHTML = taxPrice;
+
+    const profitPrice = document.getElementById("profit");
+    profitPrice.innerHTML = inputValue - taxPrice;
+  });
+});
