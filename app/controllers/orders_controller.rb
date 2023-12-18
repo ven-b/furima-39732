2dd0f2432eb53d827372d988b5f25db223fffe85
ven-b <bigstarbag@gmail.com>
2023-12-18 19:20:17 +0900

    PayJPを利用したクレジットカードの購入機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,6 +3,7 @@ class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
 
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @order_address = OrderAddress.new
     if current_user.id == @item.user_id
       redirect_to root_path
@@ -15,9 +16,11 @@ class OrdersController < ApplicationController
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render :index, status: :unprocessable_entity
     end
   end
@@ -25,11 +28,19 @@ class OrdersController < ApplicationController
 private
   
   def order_params
-    params.require(:order_address).permit(:postal_code, :area_id, :city, :street, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id )
+    params.require(:order_address).permit(:postal_code, :area_id, :city, :street, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id ,token: params[:token])
   end
 
   def set_item
     @item = Item.find(params[:item_id])
   end
 
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,9 +1,9 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :area_id, :city, :street, :building_name, :phone_number
+  attr_accessor :user_id, :item_id, :postal_code, :area_id, :city, :street, :building_name, :phone_number ,:token
 
   with_options presence: true do
-    validates :user_id, :item_id, :city, :street
+    validates :user_id, :item_id, :city, :street ,:token
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
     validates :area_id, numericality: { other_than: 1 , message: "can't be blank"}
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
